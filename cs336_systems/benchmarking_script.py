e4842d110de93dd7067d58796d1299fc082e08a0
Sergii Rykov <serenya@live.com>
2025-11-07 23:35:58 +0100

    Implement benchmarking script

---
+++ cs336_systems/benchmarking_script.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+import timeit
+from cs336_systems.model import BasicsTransformerLM
+
+
+def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_num_steps, num_steps):
+    print(f"=================Benchmark for model {size} started=================")
+    model = BasicsTransformerLM(
+            vocab_size=10000,
+            context_length=200,
+            d_model=d_model,
+            num_layers=num_layers,
+            num_heads=num_heads,
+            d_ff=d_ff,
+            rope_theta=0.5)
+    
+    x = torch.randint(1, 5, (1, 150))
+
+    for step in range(w_num_steps):
+        print(f"\rWarm-up step forward: {step}", end="")
+        y = model(x).mean()
+
+    forward_time = []
+    backward_time = []
+    for step in range(num_steps):
+        print(f"\rBenchmark step forward: {step}", end="")
+        t1 = timeit.default_timer()
+        y = model(x).mean()
+        t2 = timeit.default_timer()
+        forward_time.append(t2-t1)
+
+        print(f"\rBenchmark step backward: {step}", end="")
+        t1 = timeit.default_timer()
+        y.backward()
+        t2 = timeit.default_timer()
+        backward_time.append(t2-t1)
+        #torch.cuda.synchronize()
+    print(f"=================Benchmark for model {size} finished=================")
+    print(f"Forward average timing: {np.average(forward_time)}")
+    print(f"Forward stadard deviation timing: {np.std(np.array(forward_time))}")
+    print(f"Backward average timing: {np.average(backward_time)}")
+    print(f"Backward stadard deviation timing: {np.std(np.array(backward_time))}")
+    
+
+if __name__ == "__main__":
+    #run_basics_transformer_model(size="small", d_model=768, d_ff=3072, num_layers=12, num_heads=12, w_num_steps = 5, num_steps = 10)
+
+    run_basics_transformer_model(size="medium", d_model=1024, d_ff=4096, num_layers=24, num_heads=16, w_num_steps = 5, num_steps = 10)
+
+    """ run_basics_transformer_model(size="large", d_model=1280, d_ff=5120, num_layers=36, num_heads=20, w_num_steps = 5, num_steps = 10)
+
+    run_basics_transformer_model(size="xl", d_model=1600, d_ff=6400, num_layers=48, num_heads=25, w_num_steps = 5, num_steps = 10)
+
+    run_basics_transformer_model(size="2.7B", d_model=2560, d_ff=10240, num_layers=32, num_heads=32, w_num_steps = 5, num_steps = 10) """
