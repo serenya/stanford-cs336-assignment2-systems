1c98c55f6c0981cd6726014b34226605c2598d78
Sergii Rykov <serenya@live.com>
2025-11-11 16:36:31 +0100

    Add CUDA support

--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -3,9 +3,10 @@ import numpy as np
 import timeit
 from cs336_systems.model import BasicsTransformerLM
 
-
 def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_num_steps, num_steps):
     print(f"=================Benchmark for model {size} started=================")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     model = BasicsTransformerLM(
             vocab_size=10000,
             context_length=200,
@@ -13,9 +14,11 @@ def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_n
             num_layers=num_layers,
             num_heads=num_heads,
             d_ff=d_ff,
-            rope_theta=0.5)
+            rope_theta=0.5).to(device)
     
-    x = torch.randint(1, 5, (1, 150))
+    
+    
+    x = torch.randint(1, 5, (1, 150), device=device)
 
     for step in range(w_num_steps):
         print(f"\rWarm-up step forward: {step}", end="")
@@ -27,15 +30,17 @@ def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_n
         print(f"\rBenchmark step forward: {step}", end="")
         t1 = timeit.default_timer()
         y = model(x).mean()
+        torch.cuda.synchronize()
         t2 = timeit.default_timer()
         forward_time.append(t2-t1)
 
         print(f"\rBenchmark step backward: {step}", end="")
         t1 = timeit.default_timer()
         y.backward()
+        torch.cuda.synchronize()
         t2 = timeit.default_timer()
         backward_time.append(t2-t1)
-        #torch.cuda.synchronize()
+        
     print(f"=================Benchmark for model {size} finished=================")
     print(f"Forward average timing: {np.average(forward_time)}")
     print(f"Forward stadard deviation timing: {np.std(np.array(forward_time))}")
@@ -44,12 +49,12 @@ def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_n
     
 
 if __name__ == "__main__":
-    #run_basics_transformer_model(size="small", d_model=768, d_ff=3072, num_layers=12, num_heads=12, w_num_steps = 5, num_steps = 10)
+    run_basics_transformer_model(size="small", d_model=768, d_ff=3072, num_layers=12, num_heads=12, w_num_steps = 5, num_steps = 10)
 
     run_basics_transformer_model(size="medium", d_model=1024, d_ff=4096, num_layers=24, num_heads=16, w_num_steps = 5, num_steps = 10)
 
-    """ run_basics_transformer_model(size="large", d_model=1280, d_ff=5120, num_layers=36, num_heads=20, w_num_steps = 5, num_steps = 10)
+    run_basics_transformer_model(size="large", d_model=1280, d_ff=5120, num_layers=36, num_heads=20, w_num_steps = 5, num_steps = 10)
 
     run_basics_transformer_model(size="xl", d_model=1600, d_ff=6400, num_layers=48, num_heads=25, w_num_steps = 5, num_steps = 10)
 
-    run_basics_transformer_model(size="2.7B", d_model=2560, d_ff=10240, num_layers=32, num_heads=32, w_num_steps = 5, num_steps = 10) """
+    run_basics_transformer_model(size="2.7B", d_model=2560, d_ff=10240, num_layers=32, num_heads=32, w_num_steps = 5, num_steps = 10)
