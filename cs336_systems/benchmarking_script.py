d2f8eb8309fb2acd6b9dfffbbd88a99e71b5cd89
Sergii Rykov <serenya@live.com>
2025-11-22 00:09:08 +0100

    Implement benchmarking mixed precision

--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -2,14 +2,15 @@ import torch
 import numpy as np
 import timeit
 
+from contextlib import nullcontext
 from cs336_basics.model import BasicsTransformerLM
 from cs336_basics.nn_utils import cross_entropy
 import pandas as pd
 
-def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_num_steps, num_steps):
-    print(f"=================Benchmark for model {size} started=================")
+def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_num_steps, num_steps, use_autocast):
+    print(f"=================Benchmark for model {size} {"mixed precision" if use_autocast else "full precision"} started=================")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    
     model = BasicsTransformerLM(
             vocab_size=10000,
             context_length=256,
@@ -28,19 +29,21 @@ def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_n
 
     forward_time = []
     backward_time = []
+    autocast_context = torch.autocast(device_type='cuda') if use_autocast else nullcontext()
     for step in range(num_steps):
         print(f"\rBenchmark step forward pass: {step}", end="")
         t1 = timeit.default_timer()
-        logits = model(x)
-        if torch.cuda.is_available():
-            torch.cuda.synchronize()
+        with autocast_context:
+            logits = model(x)
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
 
-        t2 = timeit.default_timer()
-        forward_time.append(t2-t1)
+            t2 = timeit.default_timer()
+            forward_time.append(t2-t1)
 
-        print(f"\rBenchmark step backward pass: {step}", end="")
-        t1 = timeit.default_timer()
-        loss = cross_entropy(logits, y)
+            print(f"\rBenchmark step backward pass: {step}", end="")
+            t1 = timeit.default_timer()
+            loss = cross_entropy(logits, y)
         loss.backward()
         if torch.cuda.is_available():
             torch.cuda.synchronize()
@@ -49,7 +52,7 @@ def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_n
         backward_time.append(t2-t1)
     
 
-    print(f"=================Benchmark for model {size} finished=================")
+    print(f"=================Benchmark for model {size} {"mixed precision" if use_autocast else "full precision"} finished=================")
     forward_time_average = np.average(forward_time)
     forward_time_standard_deviation = np.std(np.array(forward_time))
     backward_time_average = np.average(backward_time)
@@ -65,17 +68,27 @@ def run_basics_transformer_model(size, d_model, d_ff, num_layers, num_heads, w_n
 if __name__ == "__main__":
     results = []
 
-    results.append(run_basics_transformer_model(size="small", d_model=768, d_ff=3072, num_layers=12, num_heads=12, w_num_steps = 5, num_steps = 10))
+    results.append(run_basics_transformer_model(size="small", d_model=768, d_ff=3072, num_layers=12, num_heads=12, w_num_steps = 5, num_steps = 10, use_autocast=False))
 
-    results.append(run_basics_transformer_model(size="medium", d_model=1024, d_ff=4096, num_layers=24, num_heads=16, w_num_steps = 5, num_steps = 10))
+    results.append(run_basics_transformer_model(size="medium", d_model=1024, d_ff=4096, num_layers=24, num_heads=16, w_num_steps = 5, num_steps = 10, use_autocast=False))
 
-    results.append(run_basics_transformer_model(size="large", d_model=1280, d_ff=5120, num_layers=36, num_heads=20, w_num_steps = 5, num_steps = 10))
+    results.append(run_basics_transformer_model(size="large", d_model=1280, d_ff=5120, num_layers=36, num_heads=20, w_num_steps = 5, num_steps = 10, use_autocast=False))
 
-    results.append(run_basics_transformer_model(size="xl", d_model=1600, d_ff=6400, num_layers=48, num_heads=25, w_num_steps = 5, num_steps = 10))
+    results.append(run_basics_transformer_model(size="xl", d_model=1600, d_ff=6400, num_layers=48, num_heads=25, w_num_steps = 5, num_steps = 10, use_autocast=False))
 
-    results.append(run_basics_transformer_model(size="2.7B", d_model=2560, d_ff=10240, num_layers=32, num_heads=32, w_num_steps = 5, num_steps = 10))
+    results.append(run_basics_transformer_model(size="2.7B", d_model=2560, d_ff=10240, num_layers=32, num_heads=32, w_num_steps = 5, num_steps = 10, use_autocast=False))
 
-    df = pd.DataFrame(results, columns=['Model', 'Forward Time Avg', 'Forward Time Std', 'Backward Time Avg', 'Backward Time Std'])
+    results.append(run_basics_transformer_model(size="small", d_model=768, d_ff=3072, num_layers=12, num_heads=12, w_num_steps = 5, num_steps = 10, use_autocast=True))
+
+    results.append(run_basics_transformer_model(size="medium", d_model=1024, d_ff=4096, num_layers=24, num_heads=16, w_num_steps = 5, num_steps = 10, use_autocast=True))
+
+    results.append(run_basics_transformer_model(size="large", d_model=1280, d_ff=5120, num_layers=36, num_heads=20, w_num_steps = 5, num_steps = 10, use_autocast=True))
+
+    results.append(run_basics_transformer_model(size="xl", d_model=1600, d_ff=6400, num_layers=48, num_heads=25, w_num_steps = 5, num_steps = 10, use_autocast=True))
+
+    results.append(run_basics_transformer_model(size="2.7B", d_model=2560, d_ff=10240, num_layers=32, num_heads=32, w_num_steps = 5, num_steps = 10, use_autocast=True))
+
+    df = pd.DataFrame(results, columns=['Model', 'Forward Time Avg', 'Forward Time Std', 'Backward Time Avg', 'Backward Time Std', 'Full precision'])
 
     print("\n=================Benchmark Results=================")
     print(df.to_markdown(index=False))
