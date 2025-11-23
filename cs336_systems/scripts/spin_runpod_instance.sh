79e1bf2a6838e293fb60003085b04671c01d3502
Sergii Rykov <serenya@live.com>
2025-11-23 22:46:22 +0100

    Profile memory for model 2.7B and running it on 80GB GPU server

--- cs336_systems/scripts/spin_runpod_instance.sh
+++ cs336_systems/scripts/spin_runpod_instance.sh
@@ -4,11 +4,12 @@
 echo -e '\e[1;33mStep 0 - spin up RunPod instance\e[0m'
 
 PUBLIC_KEY=$(<"$HOME/.ssh/runpod_ed25519.pub")
+JUPYTER_PASSWORD=$(openssl rand -base64 15)
 
 output=$(runpodctl create pod \
   --name "my-training-pod" \
   --templateId "runpod-torch-v280" \
-  --gpuType "NVIDIA L40S" \
+  --gpuType "NVIDIA A100 80GB PCIe" \
   --gpuCount 1 \
   --secureCloud \
   --imageName "runpod/pytorch:1.0.2-cu1281-torch280-ubuntu2404" \
@@ -16,7 +17,8 @@ output=$(runpodctl create pod \
   --volumeSize 30 \
   --ports 8888/http \
   --ports 22/tcp \
-  --env PUBLIC_KEY="$PUBLIC_KEY")
+  --env PUBLIC_KEY="$PUBLIC_KEY" \
+  --env JUPYTER_PASSWORD="$JUPYTER_PASSWORD")
 
 echo "create output: $output"
 
